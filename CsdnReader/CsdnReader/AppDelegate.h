166f89b49c92b2003f148a312eaada37521882a4
fanlv <fanlvlgh@gmail.com>
2013-01-21 21:25:41 +0800

    加入回复按钮

---
+++ CsdnReader/CsdnReader/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  CsdnReader
+//
+//  Created by Fan Lv on 13-1-8.
+//  Copyright (c) 2013年 Fan Lv. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
